c067ce26f7be561b01a68c55e6da953e4b6d4526
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-05-30 20:14:39 +0200

    ft: V0,1 computing choices

--- GAME/game.py
+++ GAME/game.py
@@ -1,6 +1,7 @@
 from enum import Enum
+from random import choice
 
-class UserChoice(Enum):
+class GameChoice(Enum):
     INVALID = -1
     PAPER = 0
     ROCK = 1
@@ -30,52 +31,88 @@ def read_user_choice():
     """
     Lee una selección del usuario (piedra, papel, tijera o salir) y la devuelve
     """
-    user_choice = UserChoice.INVALID
-    while user_choice == UserChoice.INVALID:
+    user_choice = GameChoice.INVALID
+    while user_choice == GameChoice.INVALID:
         print("Select one number: ")
-        print(f'{UserChoice.PAPER.value}. Paper')
-        print(f'{UserChoice.ROCK.value}. Rock')
-        print(f'{UserChoice.SCISSORS.value}. Scissors')
+        print(f'{GameChoice.PAPER.value}. Paper')
+        print(f'{GameChoice.ROCK.value}. Rock')
+        print(f'{GameChoice.SCISSORS.value}. Scissors')
         print(f'--------------------')
-        print(f'{UserChoice.QUIT.value}. Quit the game')
+        print(f'{GameChoice.QUIT.value}. Quit the game')
 
         # Compruebo que los datos son correctos
         try:
-            user_choice = int(input('Enter your choice: '))
+            user_choice = GameChoice(int(input('Enter your choice: ')))
         except ValueError:
-            user_choice = UserChoice.INVALID # Si no lo son, vuelvo al menú
+            user_choice = GameChoice.INVALID # Si no lo son, vuelvo al menú
 
-        # Valido lo que me ha dicho
-        if user_choice != UserChoice.INVALID:
-            break # Ok y continuamos
-        else:
-            user_choice = UserChoice.INVALID 
-    return user_choice
+    #     # Valido lo que me ha dicho
+    #     if user_choice != UserChoice.INVALID:
+    #         break # Ok y continuamos
+    #     else:
+    #         user_choice = UserChoice.INVALID 
+    # return user_choice
 
 def is_exit(user_choice):
     """
     Predicado que devuelve True si el usuario ha decidido parar y False
     si quiere seguir jugando
     """
-    return True
+    return user_choice == GameChoice.QUIT
 
 def generate_computer_choice():
     """
     Genera y devuelve una jugada al azar
     """
-    return None
+    return choice([GameChoice.PAPER, GameChoice.ROCK, GameChoice.SCISSORS])
 
 def evaluate_move(user_choice, comp_choice):
     """
     Compara las dos jugadas y devuelve un texto con el resultado
     """
-    return None
+    assert user_choice != GameChoice.INVALID and user_choice != GameChoice.QUIT
+    assert comp_choice != GameChoice.INVALID and comp_choice != GameChoice.QUIT
+    
+    result = ''
+
+    if user_choice == GameChoice.PAPER:
+        if comp_choice == GameChoice.PAPER:
+            result = "It's a tie!"
+        elif comp_choice == GameChoice.ROCK:
+            result = 'You win! Paper covers Rock'
+        else:
+            result = "I win! Scissors cut paper"
+
+    elif user_choice == GameChoice.ROCK:
+        if comp_choice == GameChoice.ROCK:
+            result = "It's a tie!"
+        elif comp_choice == GameChoice.PAPER:
+            result = 'I win! Paper covers Rock'
+        else:
+            result = "You win! Rock smashes Scissors"
+
+
+    else:
+        if comp_choice == GameChoice.SCISSORS:
+            result = "It's a tie!"
+        elif comp_choice == GameChoice.PAPER:
+            result = 'You win! Scissors cut Paper'
+        else:
+            # Rock
+            result = "I win! Rock smashes Scissors"
+
+
+
+    return result
 
 def print_result(result):
     """
     Imprime bonito el resultado
     """
-    return None
+    print('\n\n--------------------')
+    print('Game Over!')
+    print(result)
+    print('--------------------\n\n')
 
 def log_error(error):
     """
